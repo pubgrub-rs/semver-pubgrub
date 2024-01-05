0602b4094fea5e1c1a3e09612c2917ccd8b15a6c
Jacob Finkelman <jfinkelm@amazon.com>
2024-01-05 20:22:58 +0000

    test: keep crates.io data on disk

--- examples/crates-vers/src/main.rs
+++ examples/crates-vers/src/main.rs
@@ -1,62 +1,90 @@
-use std::collections::{BTreeSet, HashMap, HashSet};
+use std::collections::{BTreeSet, HashMap};
 
 use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
 use pubgrub::version_set::VersionSet;
 use rayon::prelude::*;
 use semver::{Version, VersionReq};
 use semver_pubgrub::SemverPubgrub;
+use std::io::Write;
 
-fn main() {
-    let index = crates_index::GitIndex::new_cargo_default().unwrap();
-    // index.update().unwrap();
-    let versions: BTreeSet<Version> = index
+fn get_files_from_index() {
+    println!("getting index");
+    let mut index = crates_index::GitIndex::new_cargo_default().unwrap();
+    index.update().unwrap();
+    println!("collecting versions");
+    let versions: BTreeSet<String> = index
         .crates_parallel()
         .flat_map_iter(|crt| {
             crt.unwrap()
                 .versions()
                 .iter()
-                .filter_map(|ver| {
-                    let e = ver.version().parse();
-                    match e {
-                        Ok(e) => Some(e),
-                        Err(e) => {
-                            eprintln!("{}: {e:?}", ver.version());
-                            None
-                        }
-                    }
-                })
+                .filter_map(|ver| ver.version().parse::<Version>().ok())
+                .map(|ver| ver.to_string())
                 .collect::<Vec<_>>()
                 .into_iter()
         })
         .collect();
-    let requirements: HashMap<VersionReq, SemverPubgrub> = index
+    println!("writing version file");
+    std::fs::create_dir_all("./data").unwrap();
+    let mut file = std::fs::File::create("./data/versions.csv").unwrap();
+    for ver in &versions {
+        writeln!(file, "{}", ver).unwrap();
+    }
+    drop(file);
+    println!("collecting requirements");
+    let requirements: BTreeSet<String> = index
         .crates_parallel()
         .flat_map_iter(|crt| {
             crt.unwrap()
                 .versions()
                 .iter()
                 .flat_map(|ver| ver.dependencies())
-                .map(|dep| dep.requirement().into())
+                .filter_map(|dep| dep.requirement().parse::<VersionReq>().ok())
+                .map(|req| req.to_string())
                 .collect::<Vec<_>>()
                 .into_iter()
         })
-        .collect::<HashSet<Box<str>>>()
-        .into_par_iter()
-        .filter_map(|req| {
-            let e = req.parse::<VersionReq>();
-            match e {
-                Ok(e) => Some(e),
-                Err(e) => {
-                    eprintln!("{}: {e:?}", req);
-                    None
-                }
-            }
-        })
+        .collect();
+
+    println!("writing requirements file");
+    let mut file = std::fs::File::create("./data/requirements.csv").unwrap();
+    for req in requirements {
+        writeln!(file, "{}", req).unwrap();
+    }
+    drop(file);
+}
+
+fn read_files() -> Option<(BTreeSet<Version>, HashMap<VersionReq, SemverPubgrub>)> {
+    let versions = std::fs::read_to_string("./data/versions.csv")
+        .ok()?
+        .lines()
+        .map(|ver| ver.parse().unwrap())
+        .collect();
+    let requirements = std::fs::read_to_string("./data/requirements.csv")
+        .ok()?
+        .lines()
+        .filter_map(|req| req.parse::<VersionReq>().ok())
         .map(|req| {
             let pver: SemverPubgrub = (&req).into();
             (req, pver)
         })
         .collect();
+    Some((versions, requirements))
+}
+
+fn main() {
+    // TODO: Use real argument pursing
+    let arg = std::env::args().nth(1);
+
+    if arg.is_none() || arg.as_deref() == Some("get-from-index") {
+        get_files_from_index();
+    }
+
+    let intersection = arg.is_none() || arg.as_deref() == Some("intersection");
+
+    let Some((versions, requirements)) = read_files() else {
+        panic!("no files");
+    };
 
     let style = ProgressBar::new(requirements.len() as u64).with_style(
         ProgressStyle::with_template(
@@ -87,11 +115,12 @@ fn main() {
                     assert_eq!(!req.matches(ver), neg.contains(ver));
                 }
             }
-
-            for (req2, pver2) in &requirements {
-                let inter: SemverPubgrub = pver2.intersection(&pver);
-                for ver in &versions {
-                    assert_eq!(req.matches(&ver) && req2.matches(&ver), inter.contains(ver));
+            if intersection {
+                for (req2, pver2) in &requirements {
+                    let inter: SemverPubgrub = pver2.intersection(&pver);
+                    for ver in &versions {
+                        assert_eq!(req.matches(&ver) && req2.matches(&ver), inter.contains(ver));
+                    }
                 }
             }
         })
