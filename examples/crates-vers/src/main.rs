c4dc9df0cb957148aacfc940cc21397b28c3536b
Jacob Finkelman <jfinkelm@amazon.com>
2024-01-08 21:08:10 +0000

    test: fasster crates tests

--- examples/crates-vers/src/main.rs
+++ examples/crates-vers/src/main.rs
@@ -1,6 +1,7 @@
-use std::collections::{BTreeSet, HashMap};
+use std::{cmp::min, collections::BTreeSet};
 
-use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
+use hibitset::{BitSet, BitSetLike};
+use indicatif::{ParallelProgressIterator, ProgressBar, ProgressFinish, ProgressStyle};
 use pubgrub::version_set::VersionSet;
 use rayon::prelude::*;
 use semver::{Version, VersionReq};
@@ -54,7 +55,7 @@ fn get_files_from_index() {
     drop(file);
 }
 
-fn read_files() -> Option<(BTreeSet<Version>, HashMap<VersionReq, SemverPubgrub>)> {
+fn read_files() -> Option<(Vec<Version>, Vec<(VersionReq, SemverPubgrub)>)> {
     let versions = std::fs::read_to_string("./data/versions.csv")
         .ok()?
         .lines()
@@ -86,42 +87,78 @@ fn main() {
         panic!("no files");
     };
 
-    let style = ProgressBar::new(requirements.len() as u64).with_style(
-        ProgressStyle::with_template(
-            "[Time: {elapsed}, Rate: {per_sec}, Remaining: {eta}] {wide_bar} {pos:>6}/{len:6}: {percent:>3}%",
-        )
-        .unwrap(),
-    );
+    let template =  "contains: [Time: {elapsed}, Rate: {per_sec}, Remaining: {eta}] {wide_bar} {pos:>6}/{len:6}: {percent:>3}%";
+    let style = ProgressBar::new(requirements.len() as u64)
+        .with_style(ProgressStyle::with_template(template).unwrap())
+        .with_finish(ProgressFinish::AndLeave);
 
-    requirements
-        .par_iter()
+    let requirements: Vec<_> = requirements
+        .into_par_iter()
         .progress_with(style)
-        .for_each(|(req, pver)| {
-            for ver in &versions {
-                if req.matches(&ver) != pver.contains(ver) {
+        .map(|(req, pver)| {
+            let neg = pver.complement();
+            let mut bitset = BitSet::new();
+            for (id, ver) in versions.iter().enumerate() {
+                let mat = req.matches(ver);
+                if mat != pver.contains(ver) {
                     eprintln!("{}", ver);
                     eprintln!("{}", req);
                     dbg!(&pver);
                     assert_eq!(req.matches(ver), pver.contains(ver));
                 }
-            }
-
-            let neg = pver.complement();
-            for ver in &versions {
-                if !req.matches(ver) != neg.contains(ver) {
+                if !mat != neg.contains(ver) {
                     eprintln!("{}", ver);
                     eprintln!("{}", req);
                     dbg!(&neg);
                     assert_eq!(!req.matches(ver), neg.contains(ver));
                 }
-            }
-            if intersection {
-                for (req2, pver2) in &requirements {
-                    let inter: SemverPubgrub = pver2.intersection(&pver);
-                    for ver in &versions {
-                        assert_eq!(req.matches(&ver) && req2.matches(&ver), inter.contains(ver));
-                    }
+                if mat {
+                    bitset.add(id.try_into().unwrap());
                 }
             }
+            (req, pver, bitset)
         })
+        .collect();
+
+    if intersection {
+        let template = "intersection: [Time: {elapsed}, Rate: {per_sec}, Remaining: {eta}] {wide_bar} {pos:>6}/{len:6}: {percent:>3}%";
+        let style = ProgressBar::new(requirements.len() as u64)
+            .with_style(ProgressStyle::with_template(template).unwrap())
+            .with_finish(ProgressFinish::AndLeave);
+
+        requirements
+            .par_iter()
+            .progress_with(style)
+            .enumerate()
+            .for_each(|(i, (req, pver, bs))| {
+                for (req2, pver2, bs2) in &requirements[(i + 1)..] {
+                    let inter: SemverPubgrub = pver2.intersection(&pver);
+                    assert_eq!(inter, pver.intersection(&pver2));
+                    let bs_inter: BitSet = (bs & bs2).into_iter().collect();
+                    if inter == SemverPubgrub::empty() {
+                        assert!(bs_inter.is_empty())
+                    } else if &inter == pver {
+                        assert_eq!(&bs_inter, bs)
+                    } else if &inter == pver2 {
+                        assert_eq!(&bs_inter, bs2)
+                    } else {
+                        let start = (bs | bs2)
+                            .iter()
+                            .min()
+                            .unwrap_or(versions.len().try_into().unwrap())
+                            .saturating_sub(30) as usize;
+                        let end = min(
+                            (bs | bs2).iter().max().unwrap_or(0) as usize + 30,
+                            versions.len(),
+                        );
+                        for ver in &versions[start..end] {
+                            assert_eq!(
+                                req.matches(&ver) && req2.matches(&ver),
+                                inter.contains(ver)
+                            );
+                        }
+                    }
+                }
+            })
+    }
 }
